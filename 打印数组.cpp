3e9c819beb8d6d3ce896f9a1a3e9a207701818be
DoubleKing <kevin.pqx@icloud.com>
2018-07-25 13:18:50 +0800

    add print matrix

---
+++ 打印数组.cpp
@@ -0,0 +1,32 @@
+/*
+ * Created by Peng Qixiang on 2018/7/24.
+ */
+
+/*
+ * 输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字
+ * 例如 1  2  3  4
+ *     5  6  7  8
+ *     9  10 11 12
+ *     13 14 15 16
+ * 依次打出 1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10。
+ *
+ */
+
+# include <iostream>
+# include <vector>
+
+using namespace std;
+
+vector<int> printMatrix(vector<vector<int> > matrix) {
+
+}
+
+int main(){
+
+    return 0;
+}
+
+
+
+
+
