d6183150794f6b669f28d4dacc7c36894c5bedf2
Peng Qixiang <pengqixiang@pqx.local>
2018-08-08 16:37:20 +0800

    update more than half number

--- 数组中出现次数超过一半的数字.cpp
+++ 数组中出现次数超过一半的数字.cpp
@@ -41,21 +41,43 @@ public:
         }
         return finalRes;
     }
+
+    // 若存在num，其他的数一定被num覆盖
+    int MoreThanHalfNum_Solution1(vector<int> numbers){
+        int length = numbers.size();
+        if(length == 0) return 0;
+        int res = numbers[0];
+        int count = 1;
+        for(int i = 1; i < length; i++){
+            if(numbers[i] == res) count++;
+            else count--;
+            if(count == 0) {
+                res = numbers[i];
+                count = 1;
+            }
+        }
+        // 若存在，第一个for循环后res必是这个数，但是count不一定，需要再循环一次判断
+        count = 0;
+        for(int i = 0; i < length; i++){
+            if(numbers[i] == res) count++;
+        }
+        if(count * 2 > lenth) return res;
+        else return 0;
+    }
 };
 
 int main(){
-//    Solution s = Solution();
-//    vector<int> test;
-//    test.push_back(1);
-//    test.push_back(2);
-//    test.push_back(3);
-//    test.push_back(2);
-//    test.push_back(2);
-//    test.push_back(2);
-//    test.push_back(5);
-//    test.push_back(4);
-//    test.push_back(2);
-//    cout << s.MoreThanHalfNum_Solution(test) << endl;
-    cout << 1/2 << endl;
+    Solution s = Solution();
+    vector<int> test;
+    test.push_back(1);
+    test.push_back(2);
+    test.push_back(3);
+    test.push_back(2);
+    test.push_back(2);
+    test.push_back(2);
+    test.push_back(5);
+    test.push_back(4);
+    test.push_back(2);
+    cout << s.MoreThanHalfNum_Solution(test) << endl;
     return 0;
 }
